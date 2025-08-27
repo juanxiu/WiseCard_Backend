3c19d02414e95582c89575673e691f7a4e194f5d
yeonsoo <dfh56541@gmail.com>
2025-08-27 18:59:41 +0900

    feat: 예외 처리 응답부, 공통 응답부 생성
    Signed-off-by: yeonsoo <dfh56541@gmail.com>

--- src/main/java/com/example/demo/common/exception/ApplicationException.java
+++ src/main/java/com/example/demo/common/exception/ApplicationException.java
@@ -1,4 +1,24 @@
 package com.example.demo.common.exception;
 
-public class ApplicationException {
+import com.example.demo.common.response.CodeEnum;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class ApplicationException extends RuntimeException{
+    private final CodeEnum code;
+    private final Map<String, Object> data;
+
+    public ApplicationException(CodeEnum code, String message, Map<String, Object> data){
+        super(message);
+        this.code = code;
+        this.data = data;
+    }
+
+    public ApplicationException(CodeEnum code, String message){
+        super(message);
+        this.code = code;
+        this.data = null;
+    }
 }
