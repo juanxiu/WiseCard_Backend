bcdb663e937a3988137bb9d5168f233734071ec3
yeonsoo <dfh56541@gmail.com>
2025-08-28 15:25:58 +0900

    feat: controller GetMapping 으로 변경
    Signed-off-by: yeonsoo <dfh56541@gmail.com>

--- src/main/java/com/example/demo/common/controller/sampleController.java
+++ src/main/java/com/example/demo/common/controller/sampleController.java
@@ -1,5 +1,6 @@
 package com.example.demo.common.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/sample")
 public class sampleController {
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String sampleEndpoint(
             @RequestBody List<Integer> requestBody
             ){
